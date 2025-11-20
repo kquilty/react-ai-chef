dfd079eabb12667853e66c91217f65822bec55a5
Kyle Quilty <kquilty@gmail.com>
2025-11-20 16:37:28 -0500

    Added header

---
+++ ai-chef/src/components/Header.jsx
@@ -0,0 +1,9 @@
+import iconImage from '../images/ai-chef-icon.png'
+export default function Header() {
+    return (
+        <header>
+            <img src={iconImage} alt="AI Chef Icon" />
+            <h1>AI Chef</h1>
+        </header>
+    )
+}
